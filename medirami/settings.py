ef4ac866193c68d78b11481dcd46c00ee2c0f81f
facuzavaleta <facuzavaleta890@gmail.com>
2024-06-06 20:21:19 -0300

    cors added

--- medirami/settings.py
+++ medirami/settings.py
@@ -51,9 +51,11 @@ INSTALLED_APPS = [
     'historias_clinicas',
     'recetas',
     'pedidos_laboratorio',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -62,6 +64,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'medirami.urls'
 
