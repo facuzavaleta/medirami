4ab312bfd25dd1b57e27c93db878acc728c2f3e0
Luis Lescano <luislescanologo@hotmail.com>
2024-06-30 21:41:35 -0300

    intial branch commit

--- medirami/settings.py
+++ medirami/settings.py
@@ -24,9 +24,6 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
