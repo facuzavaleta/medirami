6464133e9b90b39c9f6db404a72bd90bdd6c0c9e
facuzavaleta <facuzavaleta890@gmail.com>
2024-07-08 13:23:01 -0300

    pacientes serializer fixed

--- pacientes/serializers.py
+++ pacientes/serializers.py
@@ -1,13 +1,15 @@
 from rest_framework import serializers
 from .models import Paciente
+from users.models import CustomUser
 
 class PacienteSerializer(serializers.ModelSerializer):
     edad = serializers.ReadOnlyField()
+    user_id = serializers.PrimaryKeyRelatedField(queryset=CustomUser.objects.all(), source='user')
 
     class Meta:
         model = Paciente
         fields = [
             'id', 'nombre', 'apellido', 'sexo', 'fecha_nacimiento',
             'obra_social', 'numero_afiliado', 'dni', 'provincia',
-            'ciudad', 'medicacion', 'historia_clinica', 'edad'
+            'ciudad', 'medicacion', 'user_id', 'edad'
         ]
