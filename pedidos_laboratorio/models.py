aa0b0376b4bb0db7062b02c129471c922be9c26e
facuzavaleta89 <facuzavaleta890@gmail.com>
2024-07-30 17:57:23 -0300

    add pedidos_laboratorio

--- pedidos_laboratorio/models.py
+++ pedidos_laboratorio/models.py
@@ -1,14 +1,11 @@
 from django.db import models
+from pacientes.models import Paciente
 
 class PedidoLaboratorio(models.Model):
-    nombre_apellido = models.CharField(max_length=200)
-    dni = models.CharField(max_length=20)
-    obra_social = models.CharField(max_length=100)
-    numero_afiliado = models.CharField(max_length=100)
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
     pedido = models.TextField()
     fecha = models.DateField()
     firma = models.CharField(max_length=200)
-    codigo_identificacion = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
-        return f"Pedido de laboratorio de {self.nombre_apellido} - {self.fecha}"
+        return f"Pedido de laboratorio de {self.paciente.nombre} - {self.fecha}"
